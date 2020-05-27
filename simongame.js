034bf9013f1abecc91d4a84c0e87db47cf985101
Asheley Brown <ABrown119@humana.com>
2020-05-27 11:03:08 -0400

    JavaScript

---
+++ simongame.js
@@ -0,0 +1,23 @@
+let order = [];
+let playerOrder = []
+let flash;
+let score;
+let good;
+let compturn;
+let intervalId;
+let sound = true
+let win;
+
+const scoreCounter = document.querySelector("#score");
+const red = document.querySelector("#button1");
+const green = document.querySelector("button2");
+const yellow = document.querySelector("button3");
+const blue = document.querySelector("button4");
+const startButton = document.querySelector("#start");
+const resetButton = document.querySelector("#reset");
+
+startButton.addEventListener('click', (event), function() {
+    if (on || win) {
+        play();
+    }
+}))
